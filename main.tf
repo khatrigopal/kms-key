4db1c0181884896a089f3289c2e257ec2fcbf4c9
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-28 11:40:52 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "kms_key" {
+  source     = "./module/kms-key"
+  key_alias  = "my-kms-key"
+}
+
+variable "key_alias" {
+  description = "The alias to associate with the KMS key"
+  type        = string
+}
+
+output "kms_key_id" {
+  description = "The ID of the KMS key created by the module"
+  value       = module.kms_key.kms_key_id
+}
